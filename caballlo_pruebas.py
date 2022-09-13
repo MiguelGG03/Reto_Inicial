1860a4c01ab87ebc8ca141dd234784bb99b34ef3
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-13 13:54:26 +0200

    inyrnyod

--- caballlo_pruebas.py
+++
@@ -1,7 +0,0 @@
-"""
-n=input('NUMERO MOVS:')
-n=int(n)
-"""
-n=3
-resultado=3*(2*n)+4(2*n-1)+4(n-1)
-print(resultado)
--- caballo.py
+++ caballo.py
@@ -1,14 +1,20 @@
 def algoritmo_calculador(num):
-    resultado = 3*(2*num)+4(2*num-1)+2(2*num-2)
+    resultado = 3*(2*num)+4*(2*num-1)+2*(2*num-2)
     return resultado
 
-def main():
-        n=input('Cuantos movimientos desea usar: ')
-        n=int(n)
-        h=algoritmo_calculador(n)
-        h=str(h)
-        print(h)
-        #print("{} movimientos tiene {} caminos".format(str(n),h))
+def main():        
+        sigue=True        
+        while(sigue==True):    
+            n=input('Cuantos movimientos desea usar: ')
+            n=int(n)
+            h=algoritmo_calculador(n)
+            h=str(h)
+            print(h)
+            print("{} movimientos tiene {} caminos".format(str(n),h))
+            print("Con "+str(n)+" movimientos, tenemos "+str(h)+" posibilidades.")
+            pregunta=int(input('Desea seguir: 1=Si 0=No'))
+            if(pregunta==0):
+                sigue=False
       
 
 if __name__=='__main__':
@@ -20,18 +26,4 @@ def algoritmo_calculador(num):
 res=(20*num)+(6*(num-1))
 return int(res)
 
-
-
-
-
-
-
-
-
-
-sigue=True
-while(sigue==True):
-    print("Con "+str(n)+" movimientos, tenemos "+str(h)+" posibilidades.")
-    pregunta=int(input('Desea seguir: 1=Si 0=No'))
-    if(pregunta==0):
-        sigue=False"""
+"""
